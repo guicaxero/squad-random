e84dde5833bc3c34cdad14ab0e3df68707fc11d4
Guilherme Caxéro <caxeropl@gmail.com>
2025-08-24 16:09:45 -0300

    fix: Melhorando responsividade do grid que exibe os jogadores

--- src/components/Players/index.jsx
+++ src/components/Players/index.jsx
@@ -17,14 +17,14 @@ const PlayerList = () => {
             <div className="bg-emerald-800 text-white mb-4 p-6 rounded-2xl shadow-lg w-full max-w-4xl mx-auto">
                 <h2 className="font-bold mb-4 text-3xl text-center">Jogadores</h2>
 
-                <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+                <ul className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-2">
                 {players.map((p, index) => (
                     <li
                     key={index}
-                    className="relative bg-emerald-700 p-3 rounded-2xl flex items-center justify-center font-semibold text-xl shadow-md hover:bg-emerald-600 transition-colors"
+                    className="bg-emerald-700 p-3 rounded-2xl flex items-center justify-between font-semibold text-xl shadow-md hover:bg-emerald-600 transition-colors"
                     >
-                    {p.name}
-                        <button className="absolute right-3"
+                        <span className="truncate">{p.name}</span>
+                        <button className=""
                             onClick={() => handleRemovePlayer(p.name)}
                         >
                             <FaRegTrashAlt />
